35137836c5f6fc2783080ecf984af5a3373ae395
Yaman Mahtha <yamanmahtha@gmail.com>
2025-02-16 04:12:39 +0530

    [YAMAN] add. Custom exceptions added

---
+++ Day04/CustomException.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomException {
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        } else {
+            System.out.println("Access granted!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter your age: ");
+            int age = scanner.nextInt();
+            validateAge(age);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: Invalid input.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
